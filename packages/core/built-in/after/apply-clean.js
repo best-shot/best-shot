e2e5c54fdbda38912cde1cde253a60f599a2f4f6
Eric Chen <airkro@qq.com>
2019-03-05 09:00:49 +0800

    remove config of CleanWebpackPlugin

--- packages/core/built-in/after/apply-clean.js
+++ packages/core/built-in/after/apply-clean.js
@@ -1,6 +1,4 @@
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const { relative } = require('path');
-const { currentPath } = require('../../lib/common');
 
 const displayName = 'clean-output';
 
@@ -8,13 +6,7 @@ exports.name = displayName;
 
 exports.apply = function applyClean({ options: { serve, watch } }) {
   return chain =>
-    chain.when(!serve && !watch, config => {
-      const context = currentPath();
-      return config
-        .plugin(displayName)
-        .use(CleanWebpackPlugin, [
-          [relative(context, `${config.output.get('path')}/*`)],
-          { root: context }
-        ]);
-    });
+    chain.when(!serve && !watch, config =>
+      config.plugin(displayName).use(CleanWebpackPlugin)
+    );
 };

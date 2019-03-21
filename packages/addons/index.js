7b8bb41c0d3289a3d72a0a6794cbfabbf0acc011
Eric Chen <airkro@qq.com>
2019-03-21 09:58:31 +0800

    remove eruda

--- packages/addons/index.js
+++ packages/addons/index.js
@@ -1,6 +1,5 @@
 const cherryMap = require('./cherry-map');
 const imageAvatar = require('./image-avatar');
-const injectEruda = require('./inject-eruda');
 const removeMomentLocale = require('./remove-moment-locale');
 const replaceLodash = require('./replace-lodash');
 const srcAlias = require('./src-alias');
@@ -8,7 +7,6 @@ const srcAlias = require('./src-alias');
 module.exports = {
   cherryMap,
   imageAvatar,
-  injectEruda,
   removeMomentLocale,
   replaceLodash,
   srcAlias

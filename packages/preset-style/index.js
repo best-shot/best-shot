ce27fba47f0358824cdcc658fae46e8f3eede0f3
Eric Chen <airkro@qq.com>
2020-09-15 10:22:19 +0800

    feat(preset-style): less options

--- packages/preset-style/index.js
+++ packages/preset-style/index.js
@@ -3,12 +3,18 @@ const { relative } = require('@best-shot/core/lib/path');
 const applyScssLess = require('./lib/apply-scss-less');
 const applyStylesheet = require('./lib/apply-stylesheet');
 
-exports.apply = function applyStyle() {
+exports.apply = function applyStyle({ config: { less } }) {
   return (chain) => {
     const context = chain.get('context');
 
-    chain.batch(applyStylesheet).batch(applyScssLess);
+    chain.batch(applyStylesheet).batch(applyScssLess(less));
 
     chain.resolveLoader.modules.prepend(relative(context, module.paths[0]));
   };
+};
+
+exports.schema = {
+  less: {
+    type: 'object',
+  },
 };
--- packages/preset-style/lib/apply-scss-less.js
+++ packages/preset-style/lib/apply-scss-less.js
@@ -5,27 +5,34 @@ function addExtname(rule) {
   rule.test(regexp.add('scss', 'sass', 'less'));
 }
 
-module.exports = function applyScssLess(chain) {
-  chain.module.rule('style').batch(addExtname);
+module.exports = function applyScssLess(less) {
+  return (chain) => {
+    chain.module.rule('style').batch(addExtname);
 
-  chain.module
-    .rule('sass')
-    .test(extToRegexp({ extname: ['scss', 'sass'] }))
-    .use('resolve-url-loader')
-    .loader('resolve-url-loader')
-    .options({
-      sourceMap: !['eval', false].includes(chain.get('devtool')),
-      keepQuery: true,
-      removeCR: true,
-    })
-    .end()
-    .use('sass-loader')
-    .loader('sass-loader');
+    chain.module
+      .rule('sass')
+      .test(extToRegexp({ extname: ['scss', 'sass'] }))
+      .use('resolve-url-loader')
+      .loader('resolve-url-loader')
+      .options({
+        sourceMap: !['eval', false].includes(chain.get('devtool')),
+        keepQuery: true,
+        removeCR: true,
+      })
+      .end()
+      .use('sass-loader')
+      .loader('sass-loader');
 
-  chain.module
-    .rule('less')
-    .test(extToRegexp({ extname: ['less'] }))
-    .use('less-loader')
-    .loader('less-loader')
-    .options({ lessOptions: { rewriteUrls: 'local' } });
+    chain.module
+      .rule('less')
+      .test(extToRegexp({ extname: ['less'] }))
+      .use('less-loader')
+      .loader('less-loader')
+      .options({
+        lessOptions: {
+          rewriteUrls: 'local',
+          ...less,
+        },
+      });
+  };
 };

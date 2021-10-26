2a9540e3eea1a6cd45a2887a115c08ef5962be47
Eric <airkro@qq.com>
2021-10-26 09:46:08 +0800

    feat(preset-vue): prepare vue 3

--- packages/preset-vue/index.cjs
+++ packages/preset-vue/index.cjs
@@ -1,21 +1,33 @@
 'use strict';
 
 const extToRegexp = require('ext-to-regexp');
-const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 const { relative } = require('path');
 
 exports.name = 'preset-vue';
 
+function isVue2() {
+  try {
+    if (require('vue/package.json').version.startsWith('2')) {
+      return true;
+    }
+    return false;
+  } catch {
+    return false;
+  }
+}
+
 exports.apply = function apply() {
   return (chain) => {
     const context = chain.get('context');
+
+    const IsVue2 = isVue2();
 
     chain.module
       .rule('vue')
       .test(extToRegexp({ extname: ['vue'] }))
       .use('vue-loader')
-      .loader('vue-loader')
+      .loader(IsVue2 ? '@best-shot/vue-loader' : 'vue-loader')
       .options({
         hotReload: chain.devServer.get('hot') || false,
         compilerOptions: {
@@ -24,6 +36,10 @@ exports.apply = function apply() {
       });
 
     chain.resolveLoader.modules.prepend(relative(context, module.paths[0]));
+
+    const VueLoaderPlugin = IsVue2
+      ? require('@best-shot/vue-loader/lib/plugin')
+      : require('vue-loader/dist/plugin').default;
 
     chain.plugin('vue-loader').use(VueLoaderPlugin);
   };

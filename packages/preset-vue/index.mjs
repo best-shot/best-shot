8b23f56a8b1db2e3108bff969480362f671e5255
Eric <airkro@qq.com>
2024-03-04 14:57:56 +0800

    feat: drop vue 2 / react 16 support

--- packages/preset-vue/index.mjs
+++ packages/preset-vue/index.mjs
@@ -2,24 +2,6 @@ import { relative, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 
 import extToRegexp from 'ext-to-regexp';
-import { getPkg } from 'settingz';
-
-function isVue2() {
-  const { vue = '' } = getPkg('dependencies');
-
-  return !vue || /^[\^~]?2\./.test(vue);
-}
-
-function batch(theChain) {
-  theChain.tap((options) => ({
-    ...options,
-    esModule: false,
-    modules: {
-      ...options.modules,
-      namedExport: false,
-    },
-  }));
-}
 
 export function apply({
   config: { vue: { compilerOptions = {}, ...other } = {} },
@@ -27,14 +9,12 @@ export function apply({
   return async (chain) => {
     const context = chain.get('context');
 
-    const IsVue2 = isVue2();
-
     chain.module
       .rule('vue')
       .after('esm')
       .test(extToRegexp({ extname: ['vue'] }))
       .use('vue-loader')
-      .loader(IsVue2 ? 'vue-loader' : '@best-shot/vue-loader')
+      .loader('@best-shot/vue-loader')
       .options({
         ...other,
         hotReload: Boolean(chain.devServer.get('hot')) || false,
@@ -52,19 +32,11 @@ export function apply({
     );
 
     /* eslint-disable unicorn/no-await-expression-member */
-    const VueLoaderPlugin = IsVue2
-      ? (await import('vue-loader/lib/plugin.js')).default
-      : (await import('@best-shot/vue-loader/dist/plugin.js')).default.default;
+    const VueLoaderPlugin = (
+      await import('@best-shot/vue-loader/dist/plugin.js')
+    ).default.default;
 
     chain.plugin('vue-loader').use(VueLoaderPlugin);
-
-    if (IsVue2) {
-      const notURL = chain.module.rule('style').rule('all').oneOf('not-url');
-
-      notURL.oneOf('css-modules-by-filename').use('css-loader').batch(batch);
-
-      notURL.oneOf('css-modules-by-query').use('css-loader').batch(batch);
-    }
 
     chain.plugin('define').tap(([defines]) => [
       {

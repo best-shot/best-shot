957265e25fda838c8fa712b451354020177002bf
Eric Chen <airkro@qq.com>
2024-09-23 00:37:42 +0800

    refactor(preset-vue): import statement

--- packages/preset-vue/compiler.mjs
+++ packages/preset-vue/compiler.mjs
@@ -1,6 +1,8 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { mergeConfig } from '@best-shot/sfc-split-plugin/lib.cjs';
+import { pascalcase } from 'pascalcase';
+
 import { parse as parseRaw } from '@vue/compiler-sfc';
-// import { mergeConfig } from '@best-shot/sfc-split-plugin/lib.cjs';
-// import { pascalcase } from 'pascalcase';
 
 function isConfig(customBlock) {
   return (
@@ -24,38 +26,37 @@ export function parse(string) {
     return ast;
   }
 
-  ast.descriptor.customBlocks = ast.descriptor.customBlocks.filter(
+  const config = mergeConfig(configs);
+
+  if (
+    !(config.usingComponents && Object.keys(config.usingComponents).length > 0)
+  ) {
+    return ast;
+  }
+
+  console.log(ast);
+
+  const imports = [...Object.entries(config.usingComponents)]
+    .filter(([_, path]) => path.endsWith('.vue'))
+    .map(([key, path]) => `import ${pascalcase(key)} from '${path}';`);
+
+  const source = ast.descriptor.scriptSetup
+    ? ast.descriptor.source.replace(
+        '<script setup>',
+        ['<script setup>', ...imports].join('\n'),
+      )
+    : [
+        ast.descriptor.source,
+        '<script setup>',
+        ...imports,
+        '</script setup>',
+      ].join('\n');
+
+  const newAst = parseRaw(source);
+
+  newAst.descriptor.customBlocks = newAst.descriptor.customBlocks.filter(
     (customBlock) => !isConfig(customBlock),
   );
 
-  // const config = mergeConfig(configs);
-
-  // if (
-  //   config.usingComponents &&
-  //   Object.keys(config.usingComponents).length > 0
-  // ) {
-  // const io = [];
-  // for (const [key, path] of Object.entries(config.usingComponents)) {
-  //   if (path.endsWith('.vue')) {
-  //     io.push(`import ${pascalcase(key)} from '${path}';`);
-  //   }
-  // }
-  // if (io.length > 0) {
-  //   ast.descriptor.scriptSetup ||= {
-  //     content: ' ',
-  //     setup: true,
-  //     type: 'script',
-  //     attrs: { setup: true },
-  //   };
-  //   ast.descriptor.script ||= {
-  //     type: 'script',
-  //     attrs: {},
-  //     content: ' ',
-  //   };
-  //   console.log(ast.descriptor.scriptSetup);
-  //   // console.log([ast.descriptor]);
-  // }
-  // }
-
-  return ast;
+  return newAst;
 }

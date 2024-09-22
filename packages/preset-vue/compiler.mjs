ae65f365168a62e0ef00f3491ac0e49687e81fdc
Eric Chen <airkro@qq.com>
2024-09-23 03:17:14 +0800

    chore(preset-vue): remove config blocks

--- packages/preset-vue/compiler.mjs
+++ packages/preset-vue/compiler.mjs
@@ -1,7 +1,3 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { mergeConfig } from '@best-shot/sfc-split-plugin/lib.cjs';
-import { pascalcase } from 'pascalcase';
-
 import { parse as parseRaw } from '@vue/compiler-sfc';
 
 function isConfig(customBlock) {
@@ -14,49 +10,16 @@ function isConfig(customBlock) {
 export function parse(string) {
   const ast = parseRaw(string);
 
-  if (!ast.descriptor.customBlocks?.length) {
-    return ast;
-  }
-
-  const configs = ast.descriptor.customBlocks.filter((customBlock) =>
-    isConfig(customBlock),
-  );
-
-  if (configs.length === 0) {
-    return ast;
-  }
-
-  const config = mergeConfig(configs);
-
   if (
-    !(config.usingComponents && Object.keys(config.usingComponents).length > 0)
+    !ast.descriptor.customBlocks?.length ||
+    !ast.descriptor.customBlocks.some((customBlock) => isConfig(customBlock))
   ) {
     return ast;
   }
 
-  console.log(ast);
-
-  const imports = [...Object.entries(config.usingComponents)]
-    .filter(([_, path]) => path.endsWith('.vue'))
-    .map(([key, path]) => `import ${pascalcase(key)} from '${path}';`);
-
-  const source = ast.descriptor.scriptSetup
-    ? ast.descriptor.source.replace(
-        '<script setup>',
-        ['<script setup>', ...imports].join('\n'),
-      )
-    : [
-        ast.descriptor.source,
-        '<script setup>',
-        ...imports,
-        '</script setup>',
-      ].join('\n');
-
-  const newAst = parseRaw(source);
-
-  newAst.descriptor.customBlocks = newAst.descriptor.customBlocks.filter(
+  ast.descriptor.customBlocks = ast.descriptor.customBlocks.filter(
     (customBlock) => !isConfig(customBlock),
   );
 
-  return newAst;
+  return ast;
 }
